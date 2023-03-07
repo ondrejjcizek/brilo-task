9c637fa70e7d486540dba2d41affd96e72860dfd
Ondřej Čížek <ondrej.cizek@giant.cz>
2023-03-07 16:22:03 +0100

    SCSS variables, breakpoints, fonts + favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Jack Jackson</h1>
+<p>
+	What a bounder joseph stalin groomed sportacus testosterone trophy freestyle derek griffiths robot
+	moustache derek griffiths groomed testosterone trophy marquess of queensbury freestyle joseph
+	stalin robot moustache Nostrilis tickler sportacus what a bounder groomed?
+</p>
+
+<svelte:head>
+	<title>Brilo Task</title>
+</svelte:head>
+
+<style lang="scss">
+	h1 {
+		color: $blue;
+		font-size: 3.2rem;
+	}
+
+	p {
+		font-size: 2rem;
+	}
+
+	//Up to tablet portrait - up to 991px
+	@include down('lg') {
+		h1 {
+			color: red;
+		}
+	}
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,13 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	preprocess: [
+		vitePreprocess({
+			scss: {
+				prependData: '@use "src/app.scss" as *'
+			}
+		}),
+	],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
