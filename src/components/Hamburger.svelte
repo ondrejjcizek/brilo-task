052686a74bca1a11ba8c578fcf72f849df1db3ce
Ondřej Čížek <ondrej.cizek@giant.cz>
2023-03-07 18:20:57 +0100

    Mobile Navigation

---
+++ src/components/Hamburger.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+	export let isMenuShowing: boolean = false;
+</script>
+
+<div
+	class="Hamburger"
+	class:active={isMenuShowing}
+	on:click|preventDefault={() => {
+		isMenuShowing = !isMenuShowing;
+	}}
+	on:keydown|preventDefault={() => {
+		isMenuShowing = !isMenuShowing;
+	}}
+>
+	<span class="Hamburger-stick" />
+	<span class="Hamburger-stick" />
+	<span class="Hamburger-stick" />
+</div>
+
+<style lang="scss">
+	.Hamburger {
+		position: relative;
+		top: -1px;
+		height: 20px;
+		width: 30px;
+		cursor: pointer;
+
+		&-stick {
+			position: absolute;
+			display: block;
+			height: 2px;
+			width: 30px;
+			background: $white;
+			transition: ease opacity 0.2s, ease transform 0.3s;
+
+			&:nth-child(1) {
+				top: 0;
+			}
+
+			&:nth-child(2) {
+				top: 50%;
+				transform: translateY(-50%);
+			}
+
+			&:nth-child(3) {
+				bottom: 0;
+			}
+
+			@include up('sm') {
+				display: none;
+			}
+		}
+
+		&.active {
+			pointer-events: none;
+			position: relative;
+
+			.Hamburger-stick {
+				&:nth-child(1) {
+					top: 0;
+					transform: rotate(45deg) translate(7px, 6px);
+				}
+
+				&:nth-child(2) {
+					opacity: 0;
+				}
+
+				&:nth-child(3) {
+					transform: rotate(-45deg) translate(7px, -6px);
+				}
+			}
+		}
+	}
+</style>
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,33 +1,80 @@
 <script lang="ts">
+	import { clickOutside } from '../actions/ClickOutside';
 	import Container from './Container.svelte';
+	import Hamburger from './Hamburger.svelte';
+	let isMenuShowing: boolean = true;
 </script>
 
 <header>
 	<Container>
 		<nav>
-			<ul>
+			<ul
+				use:clickOutside={() => {
+					isMenuShowing = false;
+				}}
+				class:active={isMenuShowing}
+			>
 				<li><a href="#todo">HOME</a></li>
 				<li><a href="#todo">ABOUT ME</a></li>
 				<li><a href="#todo">MY WORK</a></li>
 			</ul>
+			<Hamburger bind:isMenuShowing />
 		</nav>
 	</Container>
 </header>
 
 <style lang="scss">
 	header {
+		position: relative;
 		background: $header-bg;
+		height: 72px;
+	}
+
+	nav {
+		display: flex;
+		align-items: center;
+		position: absolute;
+		right: 20px;
+		top: 50%;
+		transform: translateY(-50%);
+		height: 100%;
 	}
 
 	ul {
 		display: flex;
-		justify-content: flex-end;
+		justify-content: center;
 		align-items: center;
 		list-style-type: none;
 		gap: 12px;
 		font-size: 1.7rem;
 		margin: 0;
+		padding-left: 0;
 		height: 72px;
+		transition: ease transform 0.3s;
+
+		@include down('sm') {
+			display: flex;
+			position: absolute;
+			background: $header-bg;
+			right: -2px;
+			gap: 24px;
+			top: 90px;
+			width: 200px;
+			height: fit-content;
+			flex-direction: column;
+			padding: 24px;
+			border-radius: 10px;
+			transform: translateX(250px);
+		}
+		&.active {
+			@include down('sm') {
+				transform: translateX(0);
+			}
+		}
+
+		@include up('md') {
+			justify-content: flex-end;
+		}
 	}
 
 	li {
